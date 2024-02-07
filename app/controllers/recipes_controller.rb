ba26847e518eebd2e9c10b113eca5c9a46a753b4
Wala Thabet <36531358+WalaThabet@users.noreply.github.com>
2024-02-07 12:50:38 +0100

    Style devise views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,27 @@
+# frozen_string_literal: true
+
 class RecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
     if params[:search].present?
       # Determine the attribute to search by
-      search_by = params[:search_by] || 'title'  # Default to 'title' if not specified
+      search_by = params[:search_by] || 'title' # Default to 'title' if not specified
 
       # Perform the search based on the selected attribute
       case search_by
       when 'title'
-        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]), conditions: { title: params[:search] }, page: params[:page], per_page: 10)
+        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]), conditions: { title: params[:search] },
+                                                                                page: params[:page], per_page: 10)
       when 'cuisine'
-        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]), conditions: { cuisine: params[:search] }, page: params[:page], per_page: 10)
+        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]),
+                                 conditions: { cuisine: params[:search] }, page: params[:page], per_page: 10)
       when 'category'
-        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]), conditions: { category: params[:search] }, page: params[:page], per_page: 10)
+        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]),
+                                 conditions: { category: params[:search] }, page: params[:page], per_page: 10)
       when 'author'
-        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]), conditions: { author: params[:search] }, page: params[:page], per_page: 10)
+        @recipes = Recipe.search(ThinkingSphinx::Query.escape(params[:search]),
+                                 conditions: { author: params[:search] }, page: params[:page], per_page: 10)
       end
     else
       # If no search query is provided, simply paginate all recipes
